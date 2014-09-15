3f0fc833c98576934b94077bc9dd9c0c2e137fe3
Francisco Arámburo <atfm05@gmail.com>
2014-09-15 14:29:00 -0500

    Issue #7: Started the implementation.

--- app/actors/Folksonomy.scala
+++ app/actors/Folksonomy.scala
@@ -8,13 +8,27 @@ import akka.actor._
 
 object Folksonomy {
 
+  case class FolksonomyUpdate (card: String, sentiment: String, folksonomy: List[String])
+
   def props (id: String): Props = Props(new Folksonomy(id))
 }
 
 class Folksonomy (id: String) extends Actor with ActorLogging {
 
+  import akka.contrib.pattern.DistributedPubSubMediator.Publish
+  import collection.mutable.Map
+  import SentimentCard.Folksonomies
+  import Folksonomy.FolksonomyUpdate
+
+  val global = Map[String, Map[String, Int]](
+    "excellent" -> Map(),
+    "good" -> Map(),
+    "neutral" -> Map(),
+    "bad" -> Map(),
+    "terrible" -> Map())
+
   def receive = {
-    case _ => 
+    case Folksonomies(folksonomies) => 
   }
 }
 
